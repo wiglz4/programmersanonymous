09674bfaf50889b10eda4de01337556c83a17db8
google.nicknet231 <google.nicknet231@gmail.com>
2014-11-11 16:32:02 -0500

    Added data access classes

---
+++ dtabo727/webapp/webapp/SOURCE/Account.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace webapp.SOURCE
+{
+    //class is abstract so it doesnt need a constructor and can be called using a statement such as
+    //Check.Method();
+    abstract class Account
+    {
+        static String connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+
+        //Method: Insert
+        //returns true if succesfully inserted into database and false otherwise
+        //number of columns must match number of values
+        //columns and values formatted as "col1,col2,col3" "val1,val2,val3"
+        public static bool Insert(string Columns, string Values)
+        {
+            OleDbConnection connection = new OleDbConnection(connectionString);
+            if (Columns == "")
+            {
+                OleDbCommand cmd = new OleDbCommand("Insert Into [Account] Values " + "(" + Values + ")", connection);
+            }
+            else
+            {
+                OleDbCommand cmd = new OleDbCommand("Insert Into [Account]" + " (" + Columns + ") " + "Values" + " (" + Values + ")", connection);
+            }
+
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            if (result == 0) return true;
+            else return false;
+        }
+
+        //METHOD: Update
+        //returns true if update is successfully completed and false otherwise
+        //Assignments is "column1=value1,column2=value2"
+        //WhereClause is "Where some_column = some_value"
+        public static bool Update(string Assignments, string WhereClause)
+        {
+            OleDbConnection connection = new OleDbConnection(connectionString);
+            OleDbCommand cmd = new OleDbCommand("Update [Account] Set " + Assignments + " " + WhereClause, connection);
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            if (result == 0) return true;
+            else return false; 
+        }
+
+        //Method: Delete
+        //returns true if records succesfully deleted from database and false otherwise
+        //WhereClause is "Where some_column = some_value"
+        public static bool Delete(string WhereClause)
+        {
+            OleDbConnection connection = new OleDbConnection(connectionString);
+            OleDbCommand cmd = new OleDbCommand("Delete From [Account] " + WhereClause, connection);
+
+            int result = cmd.ExecuteNonQuery();
+            connection.Close();
+
+            if (result == 0) return true;
+            else return false; 
+        }
+
+        //METHOD: Query
+        //returns Dataset
+        public static DataSet Query(string WhereClause)
+        {
+            DataSet ds = new DataSet();
+
+            // Connect to the database and run the query.
+            OleDbConnection connection = new OleDbConnection(connectionString);
+            OleDbCommand cmd = new OleDbCommand("Select * From [Account]" + WhereClause, connection);
+            OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+
+            // Fill the DataSet.
+            adapter.Fill(ds);
+            connection.Close();
+
+            return ds;
+        }
+
+    }
+}
